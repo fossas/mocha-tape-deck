f2f56dfa0bb3f65950266a4b477af808ef26f401
Matt Schwartz <mattschwartz310@gmail.com>
2018-10-22 11:21:44 -0700

    remove files that should not be added

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-import * as mocha from 'mocha';
-export declare class TestCassette extends mocha.Test {
-    constructor(title: string, fn?: mocha.Func | mocha.AsyncFunc);
-    writeCassets(): void;
-    playCassette(file: string): void;
-    private getCassetteName;
-    private appendToFile;
-}
